b6baacba2245469834f1aa7597f6760fdc6e960c
Matheus Pires <matheushenriquepires99@gmail.com>
2019-10-13 10:11:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+import logo from "./assets/logo.svg";
+
+import Routes from "./routes";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container">
+            <img src={logo} alt="Air Cnc"/>
+
+            <div className="content">
+                <Routes />
+            </div>
+		</div>
+	);
 }
 
 export default App;
